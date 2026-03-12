3e4a4d6bcc709cf3d5ac1d1533b6ba103dc50a10
strestik <matejstrestik@gmail.com>
2026-03-12 13:23:36 +0100

    nahrán prázdný projekt

---
+++ it2a-databaze/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace it2a_databaze
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
