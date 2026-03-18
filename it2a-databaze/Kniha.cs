d3fce3ab1311fb52aa3af48b0b6cca3cf231fd41
strestik <matejstrestik@gmail.com>
2026-03-18 12:40:04 +0100

    Add files via upload

---
+++ it2a-databaze/Kniha.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace it2a_databaze
+{
+    class Kniha
+    {
+        public string Nazev { get; set; }
+        public string Autor { get; set; }
+        public int RokVydani { get; set; }
+        public string Zanr { get; set; }
+        public string Podzanr { get; set; }
+        public int Ks { get; set; }
+        public int Id { get; set; }
+        public string ISBN { get; set; }
+        public Kniha(string nazev, string autor, int rokVydani, string zanr, string podzanr, int ks, int id, string isbn)
+        {
+            Nazev = nazev;
+            Autor = autor;
+            RokVydani = rokVydani;
+            Zanr = zanr;
+            Podzanr = podzanr;
+            Ks = ks;
+            Id = id;
+            ISBN = isbn;
+        }
+    }
+}
