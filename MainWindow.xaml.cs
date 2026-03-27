22abb2f9027b716a2583abbb7ded8fb2e4db4afd
strestik <matejstrestik@gmail.com>
2026-03-27 23:43:46 +0100

    blooming books

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -263,6 +263,215 @@ namespace it2a_databaze
                 Knihovna.Count + 1,
                 "978-80-7662-520-4"
             ));
+
+            Knihovna.Add(new Kniha(
+                "Barva kouzel",
+                "Terry Pratchett",
+                "1993",
+                "Fantasy",
+                "Humoristická fantasy",
+                1,
+                Knihovna.Count + 1,
+                "80-85609-28-2"
+            ));
+            
+            Knihovna.Add(new Kniha(
+                "Lehké fantastično",
+                "Terry Pratchett",
+                "1993",
+                "Fantasy",
+                "Humoristická fantasy",
+                1,
+                Knihovna.Count + 1,
+                "978-80-85609-29-5"
+            ));
+            
+            Knihovna.Add(new Kniha(
+                "Čaroprávnost",
+                "Terry Pratchett",
+                "1994",
+                "Fantasy",
+                "Humoristická fantasy",
+                1,
+                Knihovna.Count + 1,
+                "978-80-7197-388-1"
+            ));
+            
+            Knihovna.Add(new Kniha(
+                "Mort",
+                "Terry Pratchett",
+                "1994",
+                "Fantasy",
+                "Humoristická fantasy",
+                1,
+                Knihovna.Count + 1,
+                "978-80-85609-55-4"
+            ));
+            
+            Knihovna.Add(new Kniha(
+                "Magický prazdroj",
+                "Terry Pratchett",
+                "1995",
+                "Fantasy",
+                "Humoristická fantasy",
+                1,
+                Knihovna.Count + 1,
+                "978-80-85609-67-7"
+            ));
+            
+            Knihovna.Add(new Kniha(
+                "Soudné sestry",
+                "Terry Pratchett",
+                "1995",
+                "Fantasy",
+                "Humoristická fantasy",
+                1,
+                Knihovna.Count + 1,
+                "80-85609-68-1"
+            ));
+            
+            Knihovna.Add(new Kniha(
+                "Pyramidy",
+                "Terry Pratchett",
+                "1995",
+                "Fantasy",
+                "Humoristická fantasy",
+                1,
+                Knihovna.Count + 1,
+                "978-80-85609-69-1"
+            ));
+            
+            Knihovna.Add(new Kniha(
+                "Stráže! Stráže!",
+                "Terry Pratchett",
+                "1996",
+                "Fantasy",
+                "Humoristická fantasy",
+                1,
+                Knihovna.Count + 1,
+                "978-80-85609-70-7"
+            ));
+            
+            Knihovna.Add(new Kniha(
+                "Erik",
+                "Terry Pratchett",
+                "1996",
+                "Fantasy",
+                "Humoristická fantasy",
+                1,
+                Knihovna.Count + 1,
+                "978-80-7197-003-3"
+            ));
+            
+            Knihovna.Add(new Kniha(
+                "Pohyblivé obrázky",
+                "Terry Pratchett",
+                "1996",
+                "Fantasy",
+                "Humoristická fantasy",
+                1,
+                Knihovna.Count + 1,
+                "978-80-7197-004-0"
+            ));
+            
+            Knihovna.Add(new Kniha(
+                "Sekáč",
+                "Terry Pratchett",
+                "1997",
+                "Fantasy",
+                "Humoristická fantasy",
+                1,
+                Knihovna.Count + 1,
+                "978-80-7197-005-7"
+            ));
+
+            Knihovna.Add(new Kniha(
+                "Máj / Kytice",
+                "K. H. Mácha, K. J. Erben",
+                "2000",
+                "Poezie",
+                "Česká klasika",
+                1,
+                Knihovna.Count + 1,
+                "978-80-86425-92-4"
+            ));
+            
+            Knihovna.Add(new Kniha(
+                "Písně kosmické",
+                "Jan Neruda",
+                "2009",
+                "Poezie",
+                "Česká klasika",
+                1,
+                Knihovna.Count + 1,
+                "978-80-7399-743-4"
+            ));
+            
+            Knihovna.Add(new Kniha(
+                "Havran a jiné básně",
+                "Edgar Allan Poe",
+                "2025",
+                "Poezie",
+                "Americká romantická poezie",
+                1,
+                Knihovna.Count + 1,
+                "978-80-277-2432-1"
+            ));
+            
+            Knihovna.Add(new Kniha(
+                "Balady",
+                "Johann Wolfgang von Goethe",
+                "1976",
+                "Poezie",
+                "Německá klasická poezie",
+                1,
+                Knihovna.Count + 1,
+                "9788074074615"
+            ));
+            
+            Knihovna.Add(new Kniha(
+                "Jáma a kyvadlo a jiné fantastické příběhy",
+                "Edgar Allan Poe",
+                "2007",
+                "Povídky",
+                "Americký horor",
+                1,
+                Knihovna.Count + 1,
+                "978-80-7388-023-1"
+            ));
+            
+            Knihovna.Add(new Kniha(
+                "Tvůrce osudu",
+                "Alice Shy",
+                "2019",
+                "Fantasy",
+                "Temná fantasy",
+                1,
+                Knihovna.Count + 1,
+                "978-80-270-5807-5"
+            ));
+            
+            Knihovna.Add(new Kniha(
+                "Korsak a zlatý meč",
+                "Jitka Komendová",
+                "2016",
+                "Fantasy",
+                "YA Fantasy",
+                1,
+                Knihovna.Count + 1,
+                "978-80-253-2885-9"
+            ));
+            
+            Knihovna.Add(new Kniha(
+                "Dobrodružství krále Artuše",
+                "Olga M. Yuste",
+                "2021",
+                "Dobrodružné",
+                "Legendy a mýty",
+                1,
+                Knihovna.Count + 1,
+                "978-80-7557-775-4"
+            ));
         }
 
         public MainWindow()
